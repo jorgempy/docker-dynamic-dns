03940561ae76295bc08db057c46c815ac89086e0
jorgeme <jorge@jmtech.com.py>
2018-07-26 18:31:57 -0400

    proper support for duckdns

--- no-ip.sh
+++ no-ip.sh
@@ -62,37 +62,40 @@ USERAGENT="--user-agent=\"no-ip shell script/1.0 mail@mail.com\""
 BASE64AUTH=$(echo '"$USER:$PASSWORD"' | base64)
 AUTHHEADER="--header=\"Authorization: $BASE64AUTH\""
 
-NOIPURL="https://$USER:$PASSWORD@www.duckdns.org/v3/update"
 
-
-if [ -n "$IP" ] || [ -n "$HOSTNAME" ]
-then
-	NOIPURL="$NOIPURL?"
-fi
-
-if [ -n "$HOSTNAME" ]
-then
-	NOIPURL="${NOIPURL}hostname=${HOSTNAME}"
-fi
-
-if [ -n "$IP" ]
-then
-	if [ -n "$HOSTNAME" ]
-	then
-		NOIPURL="$NOIPURL&"
-	fi
-	NOIPURL="${NOIPURL}myip=$IP"
-fi
-
-
-echo "$AUTHHEADER $USERAGENT $NOIPURL"
+#echo "$AUTHHEADER $USERAGENT $NOIPURL"
 
 while :
-do
+	do
+		if [ -n "$DETECTIP" ]
+	then
+		IP=$(wget -qO- "http://myexternalip.com/raw")
+	fi
 
-	RESULT=$(wget --no-check-certificate -qO- $AUTHHEADER $USERAGENT $NOIPURL)
 
+	if [ -n "$DETECTIP" ] && [ -z $IP ]
+	then
+		RESULT="Could not detect external IP."
+	else
+		case "$SERVICE" in
+	        dynu)
+
+				# TODO
+	            SERVICEURL="dynupdate.no-ip.com/nic/update"
+	            ;;
+
+	        duckdns)
+				RESULT=$(wget --no-check-certificate -qO- $USERAGENT https://www.duckdns.org/update?domains=$HOSTNAME&token=$USER[&ip=$IP)
+	            ;;
+	        *)
+				SERVICEURL="dynupdate.no-ip.com/nic/update"
 	 
+		esac
+	fi
+	#RESULT=$(wget --no-check-certificate -qO- $AUTHHEADER $USERAGENT $NOIPURL)
+	
+
+	
 	echo $RESULT
 	
 
